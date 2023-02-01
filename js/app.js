1d5af0510ddc2c1d34b772e86d2cfd18672e6fd2
Francesco Punta <puntafrancesco@gmail.com>
2023-02-01 18:10:32 +0100

    reset post call

--- js/app.js
+++ js/app.js
@@ -1,2 +1,9 @@
+// PREZZO AL KM
+const PREZZO_PER_KM = 0.21
+
 // inserire in un input il numero di km da percorrere
+const kmDaPercorrere = document.getElementById('km').value;
+console.log(kmDaPercorrere);
+
+
 
