f57623f915a8716c91757cb04cc3767ad475c051
Francesco Punta <puntafrancesco@gmail.com>
2023-02-02 10:55:39 +0100

    correzione live

--- js/app.js
+++ js/app.js
@@ -6,4 +6,45 @@ const kmDaPercorrere = document.getElementById('km').value;
 console.log(kmDaPercorrere);
 
 
+// clik
+const buttonEl = document.getElementById('btn_calc')
+console.dir(kmDaPercorrere)
+let km
 
+
+buttonEl.addEventListener('click', function(){
+    console.log('click su btn')
+
+    km = kmDaPercorrere.value
+    let kmNum = parseFloat(km)
+    console.log(kmNum, typeof kmNum)
+
+    let sconto = 0
+    let prezzoBase = 100 * PREZZO_PER_KM
+
+
+
+
+    const prezzoFinale = prezzoBase - sconto
+})
+
+
+
+
+
+
+
+
+
+
+
+// switch (color) {
+//     case '1':
+//         b
+//         break
+//     case '2':
+        
+//         break
+//     case '3':
+
+// }
