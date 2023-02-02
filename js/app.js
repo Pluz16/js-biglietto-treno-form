d201fa94dfbb2a885c9fbb66b593d6d971282142
Francesco Punta <puntafrancesco@gmail.com>
2023-02-02 11:42:08 +0100

    Correzione live

--- js/app.js
+++ js/app.js
@@ -9,20 +9,34 @@ console.log(kmDaPercorrere);
 // clik
 const buttonEl = document.getElementById('btn_calc')
 console.dir(kmDaPercorrere)
+
+const selectEl = getElementById('sconto')
+console.log(selectEl.value)
+
 let km
 
 
 buttonEl.addEventListener('click', function(){
     console.log('click su btn')
-
+// Calcolo del prezzo base
     km = kmDaPercorrere.value
     let kmNum = parseFloat(km)
     console.log(kmNum, typeof kmNum)
 
-    let sconto = 0
     let prezzoBase = 100 * PREZZO_PER_KM
 
+    console.log('prezzo base', prezzoBase)
+// Gestiamo lo sconto
+    let sconto = 0
+    const scontoSelezionato = selectEl.value
+    console.log(scontoSelezionato, typeof scontoSelezionato)
 
+    if(scontoSelezionato === '1'){
+        sconto = prezzoBase * 0.2
+    }
+    else if (scontoSelezionato === '3'){
+        sconto = prezzoBase * 0.4
+    }
 
 
     const prezzoFinale = prezzoBase - sconto
